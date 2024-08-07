db34a722610e747c9b2bfcfd5cd76eaad1b8eac8
Jaweed <kingjaweedsediqi@gmail.com>
2024-08-07 05:12:43 +0430

    home was edited

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Home = () => {
   return (
     <div>
-      Home
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolore deleniti rerum repellendus consectetur atque assumenda perferendis eos! Id exercitationem minus aperiam architecto quidem esse eum ea harum rem aliquam!
     </div>
   )
 }
